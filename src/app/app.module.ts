afeac6d5d0d59d76d34607681fd6192b6bdbad11
Robert Mayer <rwlmayer@gmail.com>
2018-01-04 19:39:49 +0100

    Added ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
 import { BmcitemlistComponent } from './bmcitemlist/bmcitemlist.component';
@@ -12,7 +14,10 @@ import { BmcitemlistComponent } from './bmcitemlist/bmcitemlist.component';
     BmcitemlistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
