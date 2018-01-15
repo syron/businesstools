0fff8ed894c8c8f6053bdf5521d7e12fb8188257
Robert Mayer <rwlmayer@gmail.com>
2018-01-15 21:37:50 +0100

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BmcitemlistComponent } from './bmcitemlist/bmcitemlist.component';
 import { BusinesstoolsapiService } from './businesstoolsapi.service';
 import { BusinessmodelcanvasComponent } from './businessmodelcanvas/businessmodelcanvas.component';
 import { BusinessmodellistComponent } from './businessmodellist/businessmodellist.component';
+
+const appRoutes: Routes = [
+  { path: 'businessmodelcanvas', component: BusinessmodellistComponent },
+  { path: 'businessmodelcanvas/:id',      component: BusinessmodelcanvasComponent },
+  { path: '',
+    redirectTo: '/businessmodelcanvas',
+    pathMatch: 'full'
+  }
+  //,
+  //{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -22,6 +34,7 @@ import { BusinessmodellistComponent } from './businessmodellist/businessmodellis
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
