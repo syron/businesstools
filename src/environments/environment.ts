694942eccc1c506049f3b96fb2d4ed7f294e701b
Robert Mayer <rwlmayer@gmail.com>
2018-01-22 22:12:33 +0100

    replaced string

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: true,
-  apiEndpoint: '__your_production_api_endpoint__',
+  apiEndpoint: '__your_debug_or_test_api_endpoint__',
   auth0: {
     clientID: '',
     domain: '',
